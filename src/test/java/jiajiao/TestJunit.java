6d8021937b06df191e8b62f8e7d87a60c72348fe
wwr <wwr@wwr-PC>
2015-07-21 23:49:16 +0800

    commit src/test/*

---
+++ src/test/java/jiajiao/TestJunit.java
@@ -0,0 +1,14 @@
+package jiajiao;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestJunit {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
